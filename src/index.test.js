68f0a0ab5a71ba53fe9fbd94c01f6720b1950b2b
Robin <robin@grownhub.nl>
2019-03-01 14:26:27 +0100

    Test whether abort is correctly called on cancellation.

--- src/index.test.js
+++ src/index.test.js
@@ -4,12 +4,22 @@ const requestFluture = require('./index');
 
 const URL = 'http://non-existing.foo.bar';
 
+const unexpectedAbort = () => {
+  throw new Error('Unexpected abort of mock request');
+};
+
 const TIMEOUT = 1000;
-const createMockRequest = (response, error = null, timeout = TIMEOUT) =>
+const createMockRequest = (
+  response,
+  error = null,
+  timeout = TIMEOUT,
+  abort = unexpectedAbort
+) =>
   jest.fn((options, cb) => {
     setTimeout(() => {
       cb(error, response);
     }, timeout);
+    return { abort };
   });
 
 const PAYLOAD = Symbol();
@@ -38,5 +48,30 @@ it('fails erronous requests', done => {
     expect(successHandler).not.toHaveBeenCalled();
     done();
   }, successHandler);
+  jest.runAllTimers();
+});
+
+it('should abort underlying requests on cancellation', done => {
+  expect.assertions(3);
+  const successHandler = jest.fn();
+  const errorHandler = jest.fn();
+  const abortHandler = jest.fn();
+  const cancelableRequest = createMockRequest(
+    PAYLOAD,
+    null,
+    TIMEOUT,
+    abortHandler
+  );
+
+  const f = requestFluture(URL, cancelableRequest);
+  const cancel = f.fork(errorHandler, successHandler);
+
+  setTimeout(() => {
+    cancel();
+    expect(abortHandler).toHaveBeenCalled();
+    expect(errorHandler).not.toHaveBeenCalled();
+    expect(successHandler).not.toHaveBeenCalled();
+    done();
+  }, 10);
   jest.runAllTimers();
 });
